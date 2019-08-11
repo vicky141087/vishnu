4a4e912008068aab8578257dadd5e66e61b61068
Gaurav Saxena <saxena.ga@hcl.com>
2019-08-11 22:57:37 +0530

    updated

--- TF New/main.tf
+++ TF New/main.tf
@@ -12,7 +12,6 @@ resource "azurerm_key_vault" "terrakeyvault" {
   location       = "West US 2"
   tenant_id		 = "189de737-c93a-4f5a-8b68-6f4ca9941912"
   resource_group_name = "${azurerm_resource_group.res-grp.name}"
-  sku_name = "standard"
 }
 
 resource "azurerm_managed_disk" "disk" {
@@ -53,12 +52,8 @@ resource "azurerm_storage_blob" "blob_storage" {
   resource_group_name    = "${azurerm_resource_group.res-grp.name}"
   storage_account_name   = "${azurerm_storage_account.storage_account.name}"
   storage_container_name = "${azurerm_storage_container.storage_container.name}"
-  location            = "West US 2"
   type = "page"
   size = 5120
-  
-  encryption_settings {
-    enabled           = true
 
     key_encryption_key {
       key_url         = "${azurerm_key_vault.terrakeyvault.vault_uri}"
